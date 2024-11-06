e31f7a28968e005a98568b69dbda0025694fc3ae
MyangSZ <unoyou@naver.com>
2024-11-06 10:59:48 +0900

    💄 계산기 버튼 스타일 추가. hover효과 추가
    버튼 hover시 색상 변경. 버슨 스타일 개선. 연산버튼 색상변경

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function appendToDisplay(value) {
 }
 
 function calculateResult() {
-    tiy{
+    try{
     display.innerText = eval(display.innerText); // 오류 처리 없이 eval 사용
     } catch {
         display.innerText = "숫자와 사칙연산 기호만 입력해주세요"
